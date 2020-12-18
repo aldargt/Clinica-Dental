672bb19b3cd1ef9773091ab26e231c79fb68d3a6
Giovanni Sahonero Pereira <sahonero31@gmail.com>
2020-12-18 01:46:24 -0400

    modificacion de estilos para el formulario de recervar citas

--- plantillas/formreserva.php
+++ plantillas/formreserva.php
@@ -14,18 +14,12 @@
   
 </head>
 <body>
-
 <header>
-<div class="ui secondary  menu " fixed-top>
-  <a class="active item" href="../">
-    ClinicaDent
-  </a>
-</div>
-  <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light">
     <a class="navbar-brand" href="../">ClinicaDent</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     </button>
-  </nav> -->
+  </nav>
 </header>
 
 <section>
